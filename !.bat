cfb3a1c022f4bb862717f3a12bf44367bbb8b4d4
Antoine Beauchamp <end2endzone@gmail.com>
2024-10-11 18:23:00 -0400

    Generated an markdown preview.

--- !.bat
+++ !.bat
@@ -1,12 +1,16 @@
 @echo off
 
-:: https://stackoverflow.com/questions/15561997/batch-file-playlist-random-sort
+call :generate_html_preview
+call :generate_md_preview
+pause
 
+
+:generate_html_preview
+echo Generating HTML preview.
 set previewfile=%cd%\preview.html
-
 if exist "%previewfile%" del "%previewfile%"
 
-:: Build file header
+:: Build HTML file header
 echo ^<!doctype html^>>%previewfile%
 echo ^<html lang=en^> >>%previewfile%
 echo ^<head^> >>%previewfile%
@@ -16,6 +20,8 @@ echo ^</head^> >>%previewfile%
 echo ^<body^> >>%previewfile%
 echo ^<p^>Preview icons:^<br^>^</p^> >>%previewfile%
 
+:: <img src="icons/01.png" style="max-width: 400px; max-height: 400px;">
+
 ::
 :: Write the list of filenames with each
 :: prefixed by a random number and a colon
@@ -24,9 +30,42 @@ setlocal EnableDelayedExpansion
 for /f %%f in ('dir /on /b icons\*.gif;icons\*.jpg;icons\*.png') do echo ^<img src="icons/%%f" style="max-width: 400px; max-height: 400px;"^>>> !previewfile!
 endlocal
 
-:: Build file footer
+:: Build HTML file footer
 echo ^</body^> >>%previewfile%
 echo ^</html^> >>%previewfile%
 
+echo HTML preview completed.
+echo.
 
-pause
+goto :eof
+
+
+
+
+:generate_md_preview
+echo Generating MD preview.
+set previewfile=%cd%\preview.md
+if exist "%previewfile%" del "%previewfile%"
+
+:: Build HTML file header
+echo Preview icons^: >%previewfile%
+echo. >>%previewfile%
+
+:: ![icons/01.png](icons/01.png)
+
+::
+:: Write the list of filenames with each
+:: prefixed by a random number and a colon
+::
+setlocal EnableDelayedExpansion
+for /f %%f in ('dir /on /b icons\*.gif;icons\*.jpg;icons\*.png') do echo ^^![%%f](icons/%%f^?raw=true) >> !previewfile!
+endlocal
+
+echo MD preview completed.
+echo.
+
+goto :eof
+
+
+
+:eof
---
+++ preview.md
@@ -0,0 +1,11 @@
+Preview icons: 
+ 
+![01.png](icons/01.png?raw=true) 
+![02.png](icons/02.png?raw=true) 
+![03.png](icons/03.png?raw=true) 
+![04.png](icons/04.png?raw=true) 
+![05.png](icons/05.png?raw=true) 
+![06a.png](icons/06a.png?raw=true) 
+![06b.png](icons/06b.png?raw=true) 
+![08b.png](icons/08b.png?raw=true) 
+![09.png](icons/09.png?raw=true) 
