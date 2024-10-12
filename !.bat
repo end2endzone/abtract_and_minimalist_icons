3b9e0dee995ff3cb3276d8dfc6a1fbf0779489ef
Antoine Beauchamp <end2endzone@gmail.com>
2024-10-11 20:26:27 -0400

    Modified script to add to README.md.

--- !.bat
+++ !.bat
@@ -1,54 +1,20 @@
 @echo off
 
-call :generate_html_preview
-call :generate_md_preview
-pause
-
-
-:generate_html_preview
-echo Generating HTML preview.
-set previewfile=%cd%\preview.html
-if exist "%previewfile%" del "%previewfile%"
-
-:: Build HTML file header
-echo ^<!doctype html^>>%previewfile%
-echo ^<html lang=en^> >>%previewfile%
-echo ^<head^> >>%previewfile%
-echo ^<meta charset=utf-8^> >>%previewfile%
-echo ^<title^>Icons preview^</title^> >>%previewfile%
-echo ^</head^> >>%previewfile%
-echo ^<body^> >>%previewfile%
-echo ^<p^>Preview icons:^<br^>^</p^> >>%previewfile%
-
-:: <img src="icons/01.png" style="max-width: 400px; max-height: 400px;">
-
-::
-:: Write the list of filenames with each
-:: prefixed by a random number and a colon
-::
-setlocal EnableDelayedExpansion
-for /f %%f in ('dir /on /b icons\*.gif;icons\*.jpg;icons\*.png') do echo ^<img src="icons/%%f" style="max-width: 400px; max-height: 400px;"^>>> !previewfile!
-endlocal
-
-:: Build HTML file footer
-echo ^</body^> >>%previewfile%
-echo ^</html^> >>%previewfile%
-
-echo HTML preview completed.
+:: Copy template file
+echo Copying template
+copy README.md.template README.md
+echo done.
 echo.
 
-goto :eof
 
-
-
-
-:generate_md_preview
+:: Extends README.md with icons
 echo Generating MD preview.
-set previewfile=%cd%\preview.md
-if exist "%previewfile%" del "%previewfile%"
+set previewfile=%cd%\README.md
 
-:: Build HTML file header
-echo Preview icons^: >%previewfile%
+:: Build MD file header
+echo. >>%previewfile%
+echo. >>%previewfile%
+echo Preview icons^: >>%previewfile%
 echo. >>%previewfile%
 
 :: ![icons/01.png](icons/01.png)
@@ -67,5 +33,7 @@ echo.
 goto :eof
 
 
+
+pause
 
 :eof
